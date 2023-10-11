b3c5f159568977cec7a9086e8eb593c0d6e4901f
Pradeep Khanal <pradeepkhanal642@gmail.com>
2023-10-11 20:20:10 +1100

    dashboard element added but no styling yet

--- src/views/dashboard/index.jsx
+++ src/views/dashboard/index.jsx
@@ -1,4 +1,210 @@
+import { Box, Button, IconButton, Typography } from "@mui/material";
+import { mockTransactions } from "../../data/mockData";
+import DownloadOutlinedIcon from "@mui/icons-material/DownloadOutlined";
+import EmailIcon from "@mui/icons-material/Email";
+import PointOfSaleIcon from "@mui/icons-material/PointOfSale";
+import PersonAddIcon from "@mui/icons-material/PersonAdd";
+import TrafficIcon from "@mui/icons-material/Traffic";
+import ProgressCircle from "../../components/ProgressCircle";
+import Title from "../../components/Title";
+import StatBox from "../../components/StatBox";
+import LineChart from "../../components/LineChart";
+import BarChart from "../../components/BarChart";
+import GeoChart from "../../components/GeoChart";
+
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  return (
+    <Box m="20px">
+      {/* HEADER */}
+      <Box display="flex" justifyContent="space-between" alignItems="center">
+        <Title title="DASHBOARD" subtitle="Welcome to your dashboard" />
+
+        <Box>
+          <Button
+            sx={{
+              fontSize: "14px",
+              fontWeight: "bold",
+              padding: "10px 20px",
+            }}
+          >
+            <DownloadOutlinedIcon sx={{ mr: "10px" }} />
+            Download Reports
+          </Button>
+        </Box>
+      </Box>
+
+      {/* GRID & CHARTS */}
+      <Box
+        display="grid"
+        gridTemplateColumns="repeat(12, 1fr)"
+        gridAutoRows="140px"
+        gap="20px"
+      >
+        {/* ROW 1 */}
+        <Box
+          gridColumn="span 3"
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+        >
+          <StatBox
+            title="12,361"
+            subtitle="Emails Sent"
+            progress="0.75"
+            increase="+14%"
+            icon={<EmailIcon sx={{ fontSize: "26px" }} />}
+          />
+        </Box>
+        <Box
+          gridColumn="span 3"
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+        >
+          <StatBox
+            title="431,225"
+            subtitle="Sales Obtained"
+            progress="0.50"
+            increase="+21%"
+            icon={<PointOfSaleIcon sx={{ fontSize: "26px" }} />}
+          />
+        </Box>
+        <Box
+          gridColumn="span 3"
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+        >
+          <StatBox
+            title="32,441"
+            subtitle="New Clients"
+            progress="0.30"
+            increase="+5%"
+            icon={<PersonAddIcon sx={{ fontSize: "26px" }} />}
+          />
+        </Box>
+        <Box
+          gridColumn="span 3"
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+        >
+          <StatBox
+            title="1,325,134"
+            subtitle="Traffic Received"
+            progress="0.80"
+            increase="+43%"
+            icon={<TrafficIcon sx={{ fontSize: "26px" }} />}
+          />
+        </Box>
+
+        {/* ROW 2 */}
+        <Box gridColumn="span 8" gridRow="span 2">
+          <Box
+            mt="25px"
+            p="0 30px"
+            display="flex "
+            justifyContent="space-between"
+            alignItems="center"
+          >
+            <Box>
+              <Typography variant="h5" fontWeight="600">
+                Revenue Generated
+              </Typography>
+              <Typography variant="h3" fontWeight="bold">
+                $59,342.32
+              </Typography>
+            </Box>
+            <Box>
+              <IconButton>
+                <DownloadOutlinedIcon sx={{ fontSize: "26px" }} />
+              </IconButton>
+            </Box>
+          </Box>
+          <Box height="250px" m="-20px 0 0 0">
+            <LineChart isDashboard={true} />
+          </Box>
+        </Box>
+        <Box gridColumn="span 4" gridRow="span 2" overflow="auto">
+          <Box
+            display="flex"
+            justifyContent="space-between"
+            alignItems="center"
+            borderBottom={`4px solid black`}
+            p="15px"
+          >
+            <Typography variant="h5" fontWeight="600">
+              Recent Transactions
+            </Typography>
+          </Box>
+          {mockTransactions.map((transaction, i) => (
+            <Box
+              key={`${transaction.txId}-${i}`}
+              display="flex"
+              justifyContent="space-between"
+              alignItems="center"
+              borderBottom={`4px solid black`}
+              p="15px"
+            >
+              <Box>
+                <Typography variant="h5" fontWeight="600">
+                  {transaction.txId}
+                </Typography>
+                <Typography>{transaction.user}</Typography>
+              </Box>
+              <Box>{transaction.date}</Box>
+              <Box p="5px 10px" borderRadius="4px">
+                ${transaction.cost}
+              </Box>
+            </Box>
+          ))}
+        </Box>
+
+        {/* ROW 3 */}
+        <Box gridColumn="span 4" gridRow="span 2" p="30px">
+          <Typography variant="h5" fontWeight="600">
+            Campaign
+          </Typography>
+          <Box
+            display="flex"
+            flexDirection="column"
+            alignItems="center"
+            mt="25px"
+          >
+            <ProgressCircle size="125" />
+            <Typography variant="h5" sx={{ mt: "15px" }}>
+              $48,352 revenue generated
+            </Typography>
+            <Typography>Includes extra misc expenditures and costs</Typography>
+          </Box>
+        </Box>
+        <Box gridColumn="span 4" gridRow="span 2">
+          <Typography
+            variant="h5"
+            fontWeight="600"
+            sx={{ padding: "30px 30px 0 30px" }}
+          >
+            Sales Quantity
+          </Typography>
+          <Box height="250px" mt="-20px">
+            <BarChart isDashboard={true} />
+          </Box>
+        </Box>
+        <Box gridColumn="span 4" gridRow="span 2" padding="30px">
+          <Typography
+            variant="h5"
+            fontWeight="600"
+            sx={{ marginBottom: "15px" }}
+          >
+            Geography Based Traffic
+          </Typography>
+          <Box height="200px">
+            <GeoChart isDashboard={true} />
+          </Box>
+        </Box>
+      </Box>
+    </Box>
+  );
 };
+
 export default Dashboard;
